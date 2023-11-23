8e19aad1bb0afc307a711832e25ade093aa05778
ischyk-y <ischyk.y@gmail.com>
2023-11-23 17:46:06 +0200

    Delete helpers.py

--- helpers.py
+++
@@ -1,6 +0,0 @@
-def get_invite_link(invite_link):
-    print(invite_link)
-    start_index = invite_link.find('+')
-    print(start_index)
-    print(invite_link[start_index:start_index+9])
-    return invite_link[start_index:start_index + 9]
